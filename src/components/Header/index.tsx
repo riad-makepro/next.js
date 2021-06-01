f5aed97195d7e4bee73d50b86f4bb4295c762fc6
Riad Younes <riad.younes@hotmail.com>
2021-06-01 16:08:07 -0300

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import { SignInButton } from "../SignInButton";
+import styles from "./styles.module.scss";
+
+
+export function Header() {
+    return(
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <img src="/images/logo.svg" alt="logo" />
+                <nav>
+                    <li>
+                        <a className={styles.active}>Home</a>
+                        <a >Posts</a>
+                    </li>
+                </nav>
+
+                <SignInButton />
+            </div>
+        </header>
+    );
+}
